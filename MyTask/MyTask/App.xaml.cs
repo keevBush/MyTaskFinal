2f28605792b9687b8aeca22b97bc7964ba78f25a
Kevin Bushiri <keevbush@gmail.com>
2021-05-17 21:01:58 +0200

    Ajoutez des fichiers projet.

---
+++ MyTask/MyTask/App.xaml.cs
@@ -0,0 +1,33 @@
+using MyTask.ViewModels;
+using MyTask.Views;
+using Prism;
+using Prism.Ioc;
+using Xamarin.Essentials.Implementation;
+using Xamarin.Essentials.Interfaces;
+using Xamarin.Forms;
+
+namespace MyTask
+{
+    public partial class App
+    {
+        public App(IPlatformInitializer initializer)
+            : base(initializer)
+        {
+        }
+
+        protected override async void OnInitialized()
+        {
+            InitializeComponent();
+
+            await NavigationService.NavigateAsync("NavigationPage/MainPage");
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterSingleton<IAppInfo, AppInfoImplementation>();
+
+            containerRegistry.RegisterForNavigation<NavigationPage>();
+            containerRegistry.RegisterForNavigation<MainPage, MainPageViewModel>();
+        }
+    }
+}
---
+++ MyTask/MyTask/Views/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿
+namespace MyTask.Views
+{
+    public partial class MainPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
